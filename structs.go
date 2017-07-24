f8f84c4b7f1ee6b9d7f10ed693b95f574a6a2f32
Jack Henschel <jackdev@mailbox.org>
2017-07-24 12:24:46 +0200

    Update Struct for Travis Webhook Payload

--- structs.go
+++ structs.go
@@ -5,59 +5,141 @@ package travishook
 * https://docs.travis-ci.com/user/notifications/#Webhooks-Delivery-Format
  */
 type WebhookPayload struct {
-	ID             int    `json:"id"`
-	Number         string `json:"number"`
-	Status         int    `json:"status"`
-	StartedAt      string `json:"started_at"`
-	FinishedAt     string `json:"finished_at"`
-	StatusMessage  string `json:"status_message"`
-	Commit         string `json:"commit"`
-	Branch         string `json:"branch"`
-	Message        string `json:"message"`
-	CompareURL     string `json:"compare_url"`
-	CommittedAt    string `json:"committed_at"`
-	CommitterName  string `json:"committer_name"`
-	CommitterEmail string `json:"committer_email"`
-	AuthorName     string `json:"author_name"`
-	AuthorEmail    string `json:"author_email"`
-	Type           string `json:"type"`
-	BuildURL       string `json:"build_url"`
-	Repository     struct {
+	ID     int    `json:"id"`
+	Number string `json:"number"`
+	Config struct {
+		Sudo     bool     `json:"sudo"`
+		Dist     string   `json:"dist"`
+		Language string   `json:"language"`
+		Python   []string `json:"python"`
+		Branches struct {
+			Only []string `json:"only"`
+		} `json:"branches"`
+		Cache struct {
+			Pip         bool     `json:"pip"`
+			Directories []string `json:"directories"`
+		} `json:"cache"`
+		Deploy struct {
+			Provider string `json:"provider"`
+			APIKey   struct {
+				Secure string `json:"secure"`
+			} `json:"api_key"`
+			App         string `json:"app"`
+			SkipCleanup bool   `json:"skip_cleanup"`
+			True        struct {
+				Branch []string `json:"branch"`
+			} `json:"true"`
+		} `json:"deploy"`
+		Notifications struct {
+			Slack struct {
+				Rooms struct {
+					Secure string `json:"secure"`
+				} `json:"rooms"`
+				OnSuccess string `json:"on_success"`
+			} `json:"slack"`
+			Webhooks string `json:"webhooks"`
+		} `json:"notifications"`
+		Install      []string `json:"install"`
+		BeforeScript []string `json:"before_script"`
+		Script       []string `json:"script"`
+		Result       string   `json:".result"`
+		GlobalEnv    []string `json:"global_env"`
+		Group        string   `json:"group"`
+	} `json:"config"`
+	Type              string      `json:"type"`
+	State             string      `json:"state"`
+	Status            int         `json:"status"`
+	Result            int         `json:"result"`
+	StatusMessage     string      `json:"status_message"`
+	ResultMessage     string      `json:"result_message"`
+	StartedAt         time.Time   `json:"started_at"`
+	FinishedAt        time.Time   `json:"finished_at"`
+	Duration          int         `json:"duration"`
+	BuildURL          string      `json:"build_url"`
+	CommitID          int         `json:"commit_id"`
+	Commit            string      `json:"commit"`
+	BaseCommit        string      `json:"base_commit"`
+	HeadCommit        string      `json:"head_commit"`
+	Branch            string      `json:"branch"`
+	Message           string      `json:"message"`
+	CompareURL        string      `json:"compare_url"`
+	CommittedAt       time.Time   `json:"committed_at"`
+	AuthorName        string      `json:"author_name"`
+	AuthorEmail       string      `json:"author_email"`
+	CommitterName     string      `json:"committer_name"`
+	CommitterEmail    string      `json:"committer_email"`
+	PullRequest       bool        `json:"pull_request"`
+	PullRequestNumber int         `json:"pull_request_number"`
+	PullRequestTitle  string      `json:"pull_request_title"`
+	Tag               interface{} `json:"tag"`
+	Repository        struct {
 		ID        int    `json:"id"`
 		Name      string `json:"name"`
 		OwnerName string `json:"owner_name"`
 		URL       string `json:"url"`
 	} `json:"repository"`
-	Config struct {
-		Notifications struct {
-			Webhooks []string `json:"webhooks"`
-		} `json:"notifications"`
-	} `json:"config"`
 	Matrix []struct {
-		ID           int         `json:"id"`
-		RepositoryID int         `json:"repository_id"`
-		Number       string      `json:"number"`
-		State        string      `json:"state"`
-		StartedAt    interface{} `json:"started_at"`
-		FinishedAt   interface{} `json:"finished_at"`
+		ID           int    `json:"id"`
+		RepositoryID int    `json:"repository_id"`
+		ParentID     int    `json:"parent_id"`
+		Number       string `json:"number"`
+		State        string `json:"state"`
 		Config       struct {
+			Sudo     bool   `json:"sudo"`
+			Dist     string `json:"dist"`
+			Language string `json:"language"`
+			Python   string `json:"python"`
+			Branches struct {
+				Only []string `json:"only"`
+			} `json:"branches"`
+			Cache struct {
+				Pip         bool     `json:"pip"`
+				Directories []string `json:"directories"`
+			} `json:"cache"`
 			Notifications struct {
-				Webhooks []string `json:"webhooks"`
+				Slack struct {
+					Rooms struct {
+						Secure string `json:"secure"`
+					} `json:"rooms"`
+					OnSuccess string `json:"on_success"`
+				} `json:"slack"`
+				Webhooks string `json:"webhooks"`
 			} `json:"notifications"`
+			Install      []string `json:"install"`
+			BeforeScript []string `json:"before_script"`
+			Script       []string `json:"script"`
+			Result       string   `json:".result"`
+			GlobalEnv    []string `json:"global_env"`
+			Group        string   `json:"group"`
+			Os           string   `json:"os"`
+			Addons       struct {
+				Deploy struct {
+					Provider string `json:"provider"`
+					APIKey   struct {
+						Secure string `json:"secure"`
+					} `json:"api_key"`
+					App         string `json:"app"`
+					SkipCleanup bool   `json:"skip_cleanup"`
+					True        struct {
+						Branch []string `json:"branch"`
+					} `json:"true"`
+				} `json:"deploy"`
+			} `json:"addons"`
 		} `json:"config"`
-		Status         interface{} `json:"status"`
-		Log            string      `json:"log"`
-		Result         interface{} `json:"result"`
-		ParentID       int         `json:"parent_id"`
+		Status         int         `json:"status"`
+		Result         int         `json:"result"`
 		Commit         string      `json:"commit"`
 		Branch         string      `json:"branch"`
 		Message        string      `json:"message"`
-		CommittedAt    string      `json:"committed_at"`
-		CommitterName  string      `json:"committer_name"`
-		CommitterEmail string      `json:"committer_email"`
+		CompareURL     string      `json:"compare_url"`
+		StartedAt      interface{} `json:"started_at"`
+		FinishedAt     interface{} `json:"finished_at"`
+		CommittedAt    time.Time   `json:"committed_at"`
 		AuthorName     string      `json:"author_name"`
 		AuthorEmail    string      `json:"author_email"`
-		CompareURL     string      `json:"compare_url"`
+		CommitterName  string      `json:"committer_name"`
+		CommitterEmail string      `json:"committer_email"`
+		AllowFailure   bool        `json:"allow_failure"`
 	} `json:"matrix"`
 }
 
